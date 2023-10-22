cba9992da9a30f322c5b99a848680ad998ee8b32
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-22 12:44:06 -0300

    Anotando as Classes de Modelo com as Anotações JPA (jakarta.persistence)

--- src/main/kotlin/me/dio/creditapplicationsystem/entity/Andress.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/entity/Andress.kt
@@ -1,7 +1,11 @@
 package me.dio.creditapplicationsystem.entity
 
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
 data class Andress(
-    var zipCode: String = "",
-    var street: String = ""
+    @Column(nullable = false) var zipCode: String = "",
+    @Column(nullable = false) var street: String = ""
 )
 
