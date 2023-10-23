d069930ee90e8e5da37f4ed9fed530890b232303
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-23 12:18:49 -0300

    Implementação da interface - CreditService (save)

--- src/main/kotlin/me/dio/creditapplicationsystem/service/impl/CreditService.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/service/impl/CreditService.kt
@@ -1,4 +1,27 @@
 package me.dio.creditapplicationsystem.service.impl
 
-class CreditService {
+import me.dio.creditapplicationsystem.entity.Credit
+import me.dio.creditapplicationsystem.repository.CreditRepository
+import me.dio.creditapplicationsystem.service.ICreditService
+import org.springframework.stereotype.Service
+
+@Service
+class CreditService(
+    private val creditRepository: CreditRepository,
+    private val customerService: CustomerService
+) : ICreditService {
+    override fun save(credit: Credit): Credit {
+        credit.apply {
+            customer = customerService.findById(credit.customer?.id!!)
+        }
+        return this.creditRepository.save(credit)
+    }
+
+    override fun findById(creditId: Long): Credit {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(creditId: Long) {
+        TODO("Not yet implemented")
+    }
 }
