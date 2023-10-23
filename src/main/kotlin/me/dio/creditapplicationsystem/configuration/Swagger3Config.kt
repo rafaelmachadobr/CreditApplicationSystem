24b7a926fbc2d71333c126fc79a89651eaa95360
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-23 14:49:25 -0300

    Estilizando a documentação feita com o Swagger UI

--- src/main/kotlin/me/dio/creditapplicationsystem/configuration/Swagger3Config.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/configuration/Swagger3Config.kt
@@ -1,16 +1,27 @@
 package me.dio.creditapplicationsystem.configuration
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@OpenAPIDefinition(
+    info = Info(
+        title = "Sistema de Avaliação de Crédito",
+        version = "1.0",
+        description = "API Rest para um Sistema de Analise de Solicitação de Crédito"
+    ),
+    servers = [Server(url = "http://localhost:8080")]
+)
 class Swagger3Config {
     @Bean
     fun publicApi(): GroupedOpenApi? {
         return GroupedOpenApi.builder()
             .group("springcreditapplicationsystem-public")
-            .pathsToMatch("/api/customer/**", "/api/credits/**")
+            .pathsToMatch("/api/customers/**", "/api/credits/**")
             .build()
     }
 }
