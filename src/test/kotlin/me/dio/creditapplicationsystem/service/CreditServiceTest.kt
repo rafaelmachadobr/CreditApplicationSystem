155d3425da1559c4acca2ec7c4e8c742750f11f8
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-23 14:58:04 -0300

    Configurando o application-test.properties

---
+++ src/test/kotlin/me/dio/creditapplicationsystem/service/CreditServiceTest.kt
@@ -0,0 +1,12 @@
+package me.dio.creditapplicationsystem.service
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension::class)
+class CreditServiceTest {
+
+}
