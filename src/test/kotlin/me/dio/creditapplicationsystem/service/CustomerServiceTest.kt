1ef833ae63b9b151ab2ae97f8684cbd94e99b2d9
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-23 15:59:29 -0300

    Teste Unitário: CustomerService - save

---
+++ src/test/kotlin/me/dio/creditapplicationsystem/service/CustomerServiceTest.kt
@@ -0,0 +1,62 @@
+package me.dio.creditapplicationsystem.service
+
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import me.dio.creditapplicationsystem.entity.Address
+import me.dio.creditapplicationsystem.entity.Customer
+import me.dio.creditapplicationsystem.repository.CustomerRepository
+import me.dio.creditapplicationsystem.service.impl.CustomerService
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
+
+@ActiveProfiles("test")
+@ExtendWith(MockKExtension::class)
+class CustomerServiceTest {
+    @MockK
+    lateinit var customerRepository: CustomerRepository
+
+    @InjectMockKs
+    lateinit var customerService: CustomerService
+
+    @Test
+    fun `should create customer`() {
+        val fakeCustomer: Customer = buildCustomer()
+        every { customerRepository.save(any()) } returns fakeCustomer
+
+        val actual: Customer = customerService.save(fakeCustomer)
+
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isSameAs(fakeCustomer)
+        verify(exactly = 1) { customerRepository.save(fakeCustomer) }
+    }
+
+    private fun buildCustomer(
+        firstName: String = "Rafael",
+        lastName: String = "Machado",
+        cpf: String = "28475934625",
+        email: String = "rafael@gmail.com",
+        password: String = "12345",
+        zipCode: String = "12345",
+        street: String = "Rua do Rafael",
+        income: BigDecimal = BigDecimal.valueOf(1000.0),
+        id: Long = 1L
+    ) = Customer(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street,
+        ),
+        income = income,
+        id = id
+    )
+}
