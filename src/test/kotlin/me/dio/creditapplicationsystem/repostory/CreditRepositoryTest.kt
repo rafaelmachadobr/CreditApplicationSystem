095dcdf168b4041949734f769d7a2cdc7a81384f
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-23 22:39:07 -0300

    Teste de Integração: Configurando a classe CreditRepositoryTest

---
+++ src/test/kotlin/me/dio/creditapplicationsystem/repostory/CreditRepositoryTest.kt
@@ -0,0 +1,19 @@
+package me.dio.creditapplicationsystem.repostory
+
+import me.dio.creditapplicationsystem.repository.CustomerRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CreditRepositoryTest {
+    @Autowired
+    lateinit var customerRepository: CustomerRepository
+
+    @Autowired
+    lateinit var testEntityManager: TestEntityManager
+}
--- src/test/kotlin/me/dio/creditapplicationsystem/service/CreditServiceTest.kt
+++ src/test/kotlin/me/dio/creditapplicationsystem/service/CreditServiceTest.kt
@@ -1,12 +1,24 @@
 package me.dio.creditapplicationsystem.service
 
-import org.junit.jupiter.api.Assertions.*
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import me.dio.creditapplicationsystem.repository.CreditRepository
+import me.dio.creditapplicationsystem.service.impl.CreditService
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.test.context.ActiveProfiles
 
-@ActiveProfiles("test")
-@ExtendWith(MockitoExtension::class)
+@ExtendWith(MockKExtension::class)
 class CreditServiceTest {
+    @MockK
+    lateinit var creditRepository: CreditRepository
 
+    @InjectMockKs
+    lateinit var creditService: CreditService
+
+    @Test
+    fun `should create credit`() {
+        TODO("Not yet implemented")
+    }
 }
